ac948e88e4a8fd76c6ada35571a234a30aa421b5
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-10 14:00:14 -0500

    GreeterTest: use green color for lambda greeting output, black for outer implementation and blue for anonymous class implementation.

--- src/test/java/com/exebar/poc/java/lambdas/GreeterTest.java
+++ src/test/java/com/exebar/poc/java/lambdas/GreeterTest.java
@@ -5,19 +5,34 @@ import org.junit.jupiter.api.Test;
 
 class GreeterTest {
 
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+
     @Test
     void test() {
         Greeter greeter = new Greeter();
 
-        Greeting englishGreeting = () -> System.out.println("Hello!");
-        Greeting russianGreeting = () -> System.out.println("Привет!");
+        Greeting englishGreetingAsLambda = () -> System.out.println(ANSI_GREEN + "Hello!" + ANSI_RESET);
+        Greeting russianGreetingAsLambda = () -> System.out.println(ANSI_GREEN + "Привет!" + ANSI_RESET);
 
-        Greeting copyOfEnglishGreeting = new EnglishGreeting();
-        Greeting copyOfRussianGreeting = new RussianGreeting();
+        Greeting englishGreetingAsOuterImplementation = new EnglishGreeting();
+        Greeting russianGreetingAsOuterImplementation = new RussianGreeting();
 
-        greeter.greet(englishGreeting);
-        greeter.greet(copyOfEnglishGreeting);
-        greeter.greet(russianGreeting);
-        greeter.greet(copyOfRussianGreeting);
+        Greeting englishGreetingAsAnonymousClass = new Greeting() {
+            @Override
+            public void perform() {
+                System.out.println(ANSI_BLUE + "Hello!" + ANSI_RESET);
+            }
+        };
+
+        greeter.greet(englishGreetingAsLambda);
+        greeter.greet(englishGreetingAsOuterImplementation);
+        greeter.greet(englishGreetingAsAnonymousClass);
+
+        greeter.greet(russianGreetingAsLambda);
+        greeter.greet(russianGreetingAsOuterImplementation);
     }
 }
