bc4aeac8ae7868bd0b5b593528dc9c404f686416
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-10 12:58:59 -0500

    GreeterTest: add copies of English and Russian greetings instantiated as classes for better visual comparison between functional (lambda) approach and traditional object oriented approach.

--- src/test/java/com/exebar/poc/java/lambdas/GreeterTest.java
+++ src/test/java/com/exebar/poc/java/lambdas/GreeterTest.java
@@ -8,9 +8,16 @@ class GreeterTest {
     @Test
     void test() {
         Greeter greeter = new Greeter();
+
         Greeting englishGreeting = () -> System.out.println("Hello!");
         Greeting russianGreeting = () -> System.out.println("Привет!");
+
+        Greeting copyOfEnglishGreeting = new EnglishGreeting();
+        Greeting copyOfRussianGreeting = new RussianGreeting();
+
         greeter.greet(englishGreeting);
+        greeter.greet(copyOfEnglishGreeting);
         greeter.greet(russianGreeting);
+        greeter.greet(copyOfRussianGreeting);
     }
 }
