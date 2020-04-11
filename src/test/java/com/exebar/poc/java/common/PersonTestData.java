f55d80952b4881158c3352f39af3c26293cd2dfd
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-11 14:35:33 -0500

    Add new PersonTestData class to provision data for testing

---
+++ src/test/java/com/exebar/poc/java/common/PersonTestData.java
@@ -0,0 +1,16 @@
+package com.exebar.poc.java.common;
+
+public class PersonTestData {
+
+    public static Person mikeWalsh() {
+        return new Person("Mike", "Walsh", 25);
+    }
+
+    public static Person sumitChaudhari() {
+        return new Person("Sumit", "Chaudhari", 34);
+    }
+
+    public static Person jakeHillis() {
+        return new Person("Jacob", "Hillis", 31);
+    }
+}
