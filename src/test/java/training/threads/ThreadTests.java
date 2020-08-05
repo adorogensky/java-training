1308b9f1ee6ab32fd35b0bfa0d5e7b237b2ff7cf
Alex Dorogensky <alex@exebar.com>
2020-08-05 09:27:56 -0500

    split ThreadTests => ThreadTests and ThreadStateTests

--- src/test/java/training/threads/ThreadTests.java
+++ src/test/java/training/threads/ThreadTests.java
@@ -2,9 +2,6 @@ package training.threads;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class ThreadTests {
 
     /**
@@ -17,119 +14,5 @@ public class ThreadTests {
     public void testThreadId() {
         long newThreadId = new Thread().getId();
         System.out.printf("New thread ID is %d\n", newThreadId);
-    }
-
-
-    /**
-     * NEW - has not yet started
-     * RUNNABLE - executing in the JVM but may be waiting for operating system resources
-     * TERMINATED - completed execution
-     */
-    @Test
-    public void testThreadStates_beforeThreadStarted_afterThreadStarted_afterThreadFinished() throws Exception {
-        Thread t = new Thread(
-            () -> {
-                System.out.println("Thread state after start: " + Thread.currentThread().getState());
-                try {
-                    Thread.sleep(Long.MAX_VALUE);
-                } catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                    System.out.println("Thread state after interrupt: " + Thread.currentThread().getState());
-                }
-            }
-        );
-
-        System.out.println("Thread state before start: " + t.getState());
-        t.start();
-        t.interrupt();
-        t.join();
-        System.out.println("Thread state after it is finished: " + t.getState());
-    }
-
-    /*
-        BLOCKED
-     */
-    @Test
-    public void testThreadStates_beforeSynchronizedBlock() throws Exception {
-        Thread t = new Thread(
-            () -> {
-                synchronized (this) { }
-            }
-        );
-
-        synchronized (this) {
-            t.start();
-            Thread.sleep(100);
-            System.out.printf(
-                "Thread state before synchronized block: %s\n", t.getState()
-            );
-        }
-    }
-
-    /*
-        WAITING
-     */
-    @Test
-    public void testThreadStates_beforeReentrantLock_lock() throws Exception {
-        final Lock monitor = new ReentrantLock();
-        monitor.lock();
-
-        Thread t = new Thread(monitor::lock);
-
-        t.start();
-        Thread.sleep(100);
-        System.out.printf(
-            "Thread state before ReentrantLock.lock(): %s\n", t.getState()
-        );
-    }
-
-    /*
-        WAITING
-     */
-    @Test
-    public void testThreadStates_beforeWait() throws Exception {
-        Thread t = new Thread(
-            () -> {
-                synchronized (this) {
-                    try {
-                        this.wait();
-                    } catch (InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-        );
-
-        t.start();
-        Thread.sleep(100);
-        System.out.printf(
-            "Thread state before wait(): %s\n", t.getState()
-        );
-    }
-
-    /*
-      TIMED_WAITING
-     */
-    @Test
-    public void testThreadStates_beforeTimedWait() throws Exception {
-        Thread t = new Thread(
-            () -> {
-                synchronized (this) {
-                    try {
-                        this.wait(500);
-                    } catch (InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-        );
-
-        t.start();
-        Thread.sleep(100);
-        System.out.printf(
-            "Thread state before wait(): %s\n", t.getState()
-        );
     }
 }
