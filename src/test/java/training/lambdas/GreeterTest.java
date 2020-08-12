d5f10bed721efc9eabbad77a76ac28b1ea05ca68
Alex Dorogensky <alex@exebar.com>
2020-08-12 08:14:58 -0500

    refactor GreeterTest and new tests

--- src/test/java/training/lambdas/GreeterTest.java
+++ src/test/java/training/lambdas/GreeterTest.java
@@ -2,58 +2,60 @@ package training.lambdas;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 class GreeterTest {
 
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
-    private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_BLUE = "\u001B[34m";
 
-    @Test
-    void testVoidLambdaFunctions() {
-        Greeter greeter = new Greeter();
-
-        Greeting englishGreetingAsLambda = () -> System.out.println(ANSI_GREEN + "Hello!" + ANSI_RESET);
-        Greeting russianGreetingAsLambda = () -> System.out.println(ANSI_GREEN + "Привет!" + ANSI_RESET);
-
-        Greeting englishGreetingAsOuterImplementation = new EnglishGreeting();
-        Greeting russianGreetingAsOuterImplementation = new RussianGreeting();
-
-        Greeting englishGreetingAsAnonymousClass = new Greeting() {
-            @Override
-            public void perform() {
-                System.out.println(ANSI_BLUE + "Hello!" + ANSI_RESET);
-            }
-        };
-
-        greeter.greet(englishGreetingAsLambda);
-        greeter.greet(englishGreetingAsOuterImplementation);
-        greeter.greet(englishGreetingAsAnonymousClass);
-
-        greeter.greet(russianGreetingAsLambda);
-        greeter.greet(russianGreetingAsOuterImplementation);
+    private void println(String text, String color) {
+        System.out.println(color + text + ANSI_RESET);
     }
 
     @Test
-    void testReturnValueLambdaFunctionsForms() {
-        Function<Integer, Integer> squaredLambdaExplicitParamTypeAndReturn = (Integer i) -> { return i * i; };
-        Function<Integer, Integer> squaredLambdaExplicitParamTypeAndImplicitReturn = (Integer i) -> i * i;
-        Function<Integer, Integer> squaredLambdaImplicitParamTypeAndExplicitReturn = (i) -> { return i * i; };
-        Function<Integer, Integer> squaredLambdaImplicitParamTypeAndReturn = (i) -> i * i;
-        Function<Integer, Integer> squaredLambdaImplicitSimpleParamTypeAndReturn = i -> i * i;
+    void testNoParametersLambdaFunctionsThatReturnVoid() {
+        Greeter greeter = new Greeter();
 
+        greeter.greet(() -> println("Hello!", ANSI_BLUE));
+        greeter.greet(new EnglishGreeting());
+        greeter.greet(new Greeting() {
+            @Override
+            public void perform() {
+                println( "Hello!", ANSI_BLUE);
+            }
+        });
+
+        greeter.greet(() -> { println( "Привет!", ANSI_GREEN); });
+        greeter.greet(new RussianGreeting());
+    }
+
+    @Test
+    void testOneParameterLambdaFunctionsThatReturnValue() {
         Stream.of(
-                squaredLambdaExplicitParamTypeAndImplicitReturn,
-                squaredLambdaExplicitParamTypeAndReturn,
-                squaredLambdaImplicitParamTypeAndExplicitReturn,
-                squaredLambdaImplicitParamTypeAndReturn,
-                squaredLambdaImplicitSimpleParamTypeAndReturn
+            (Function<Integer, Integer>) (Integer i) -> { return i * i; },
+            (Integer i) -> { return i * i; },
+            (Integer i) -> i * i,
+            (i) -> { return i * i; },
+            (i) -> i * i,
+            i -> i * i
         ).forEach(
-            squared -> System.out.printf("5 squared is = %2d\n", squared.apply(5))
+            squared -> System.out.printf("5 * 5 = %2d\n", squared.apply(5))
+        );
+    }
+
+    @Test
+    void testTwoParametersLambdaFunctionsThatReturnValue() {
+        Stream.of(
+            (BiFunction<Integer, Integer, Integer>) (i, j) -> i + j,
+            (i, j) -> i + j,
+            Integer::sum,
+            (Integer i, Integer j) -> i + j
+        ).forEach(
+            sum -> System.out.printf("2 + 3 = %2d\n", sum.apply(2, 3))
         );
     }
 }
