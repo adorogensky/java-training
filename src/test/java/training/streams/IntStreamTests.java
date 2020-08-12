0f35863ff60abd96d417ee7ca54bfbccc9c7619a
Alex Dorogensky <alex@exebar.com>
2020-08-12 15:44:29 -0500

    refactor IntStreamTests

--- src/test/java/training/streams/IntStreamTests.java
+++ src/test/java/training/streams/IntStreamTests.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 // of(2), ofNullable, empty
 // map, mapToInt, mapToLong, mapToDouble
@@ -65,12 +65,12 @@ class IntStreamTests {
 
     @Test
     void testMax() {
-        assertEquals(4, IntStream.of(numbers).max().getAsInt());
+        assertEquals(4, IntStream.of(numbers).max().orElseThrow(() -> new RuntimeException("empty stream")));
     }
 
     @Test
     void testMin() {
-        assertEquals(1, IntStream.of(numbers).min().getAsInt());
+        assertEquals(1, IntStream.of(numbers).min().orElseThrow(() -> new RuntimeException("empty stream")));
     }
 
 
@@ -83,18 +83,20 @@ class IntStreamTests {
 
     @Test
     void testIterate() {
-        Stream.iterate(0, i -> i + 1).limit(numbers.length).forEach(
-            i -> System.out.println(numbers[i])
+        IntStream.iterate(0, i -> i + 1).limit(numbers.length).forEach(
+            i -> System.out.print(numbers[i] + " ")
         );
 
-        Stream.iterate(0, i -> i < numbers.length, i -> i + 1).forEach(
-            i -> System.out.println(numbers[i])
+        System.out.println();
+
+        IntStream.iterate(0, i -> i < numbers.length, i -> i + 1).forEach(
+            i -> System.out.print(numbers[i] + " ")
         );
     }
 
     @Test
     void testGenerate_printRandomIntegers() {
-        Stream.generate(() -> new Random().nextInt(10)).limit(5).forEach(System.out::println);
+        IntStream.generate(() -> new Random().nextInt(10)).limit(5).forEach(System.out::println);
     }
 
     @Test
@@ -103,34 +105,25 @@ class IntStreamTests {
     }
 
     @Test
-    void printFibbonaciNumbers() {
-        // 0, 1 => 0, 1, 1 => 0, 1, 1, 2 => 0, 1, 1, 2, 3 => 0, 1, 1, 2, 3, 5
-        Stream.iterate(
-            new int[] { 0, 1 },
-            twoPreviousNumbers -> new int[] { twoPreviousNumbers[1], twoPreviousNumbers[0] + twoPreviousNumbers[1] }
-        ).limit(10).mapToInt(
-            twoNumbers -> twoNumbers[0]
-        ).forEach(System.out::println);
-    }
-
-    @Test
-    void printNumbersSeparatedBySpace() {
+    void testCollect_toLinkedList() {
         List<Integer> numbersList = IntStream.of(numbers).boxed().collect(toList());
         LinkedList<Integer> numbersLinkedList = IntStream.of(numbers).boxed().collect(
-            toCollection(LinkedList::new)
+                toCollection(LinkedList::new)
         );
 
         assertTrue(numbersList.containsAll(numbersLinkedList));
         assertTrue(numbersLinkedList.containsAll(numbersList));
-
-        assertEquals("1 2 3 4", numbersList.stream().map(i -> "" + i).collect(joining(" ")));
-        assertEquals("1 2 3 4", numbersList.stream().map(i -> "" + i).reduce("", (x, y) -> x + " " + y).trim());
     }
 
     @Test
-    void printIndexAndNameMap() {
-        String[] names = {"alex", "nancy", "john"};
+    void testCollect_joining() {
+        assertEquals("1 2 3 4", IntStream.of(numbers).mapToObj(i -> "" + i).collect(joining(" ")));
+        assertEquals("1 2 3 4", IntStream.of(numbers).mapToObj(i -> "" + i).reduce("", (x, y) -> x + " " + y).trim());
+    }
 
+    @Test
+    void testCollect_toMap() {
+        String[] names = {"alex", "nancy", "john"};
         Stream.iterate(0, i -> i + 1).limit(names.length).collect(
             toMap(Function.identity(), i -> names[i])
         ).forEach(
@@ -139,7 +132,25 @@ class IntStreamTests {
     }
 
     @Test
-    void printNamesAndAppendUnknown() {
+    void testCollect_toMap_merge() {
+        String[] names = {"alex", "nancy", "john"};
+
+        Map<Integer, List<String>> namesByLength = Arrays.stream(names).collect(
+            toMap(
+                String::length,
+                Arrays::asList,
+                (oldValue, newValue) ->
+                    Stream.of(oldValue, newValue).flatMap(Collection::stream).collect(toList())
+            )
+        );
+
+        assertEquals(2, namesByLength.keySet().size());
+        assertEquals(asList("alex", "john"), namesByLength.get(4));
+        assertEquals(singletonList("nancy"), namesByLength.get(5));
+    }
+
+    @Test
+    void testCollectingAndThen() {
         String[] names = { "alex", "nancy", "john" };
         Arrays.stream(names).collect(
             collectingAndThen(toList(), list -> { list.add("unknown"); return list; })
@@ -147,16 +158,22 @@ class IntStreamTests {
     }
 
     @Test
-    void combineWordsIntoSentence() {
+    void testJoining() {
         String[] words = { "hello", "this", "world" };
         assertEquals(
             "hello this world.",
-            Arrays.stream(words).collect(Collectors.joining(" ", "", "."))
+            Arrays.stream(words).collect(joining(" ", "", "."))
         );
     }
 
+//    @Test
+//    void testMapping() {
+//        IntStream.of(numbers).collect(toList());
+//    }
+
+
     @Test
-    void printNumbersStatistics() {
+    void testSummarizingDouble() {
         DoubleSummaryStatistics statistics = IntStream.of(numbers).boxed().collect(summarizingDouble(x -> x));
         assertEquals(1, statistics.getMin());
         assertEquals(4, statistics.getMax());
@@ -170,4 +187,18 @@ class IntStreamTests {
         assertEquals(10, IntStream.of(numbers).boxed().collect(summingInt(x -> x)));
         assertEquals(2.5, IntStream.of(numbers).boxed().collect(averagingDouble(x -> x)));
     }
+
+    @Test
+    void printFibbonaciNumbers() {
+        // [0, 1] => 0 => [1, 1] => 1 => [1, 2] => 1 => [2, 3] => 2 => [3, 5] => 3 => [5, 8] => 5 => ...
+        // 0, 1, 1, 1, 2, 3, 5
+
+        Stream.iterate(
+                new int[] { 0, 1 },
+                twoPreviousNumbers -> new int[] { twoPreviousNumbers[1], twoPreviousNumbers[0] + twoPreviousNumbers[1] }
+        ).limit(10).mapToInt(
+                twoNumbers -> twoNumbers[0]
+        ).forEach(System.out::println);
+    }
+
 }
