3b86de5a34b7f681eb6fe69f5248b9ffdcead517
Alex Dorogensky <alex@exebar.com>
2020-08-13 19:33:04 -0500

    ThreadLocalTests: refactor

--- src/test/java/training/threads/ThreadLocalTests.java
+++ src/test/java/training/threads/ThreadLocalTests.java
@@ -2,46 +2,27 @@ package training.threads;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.concurrent.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ThreadLocalTests {
 
-    private static class Task implements Callable<String> {
-
-        private static final ThreadLocal<Long> threadLocal = ThreadLocal.withInitial(
-            () -> Thread.currentThread().getId()
-        );
-
-        private final String name;
-
-        private Task(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String call() {
-            return name + threadLocal.get();
-        }
-    }
+    private static final ThreadLocal<String> threadId = ThreadLocal.withInitial(
+        () -> "unknown"
+    );
 
     @Test
     public void test() throws Exception {
-        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        assertEquals("unknown", threadId.get());
+        threadId.set("main");
 
-        threadPool.invokeAll(
-            Arrays.asList(
-                new Task("A"),
-                new Task("B")
-            )
-        ).forEach(
-            taskResult -> {
-                try {
-                    System.out.println(taskResult.get());
-                } catch (InterruptedException | ExecutionException ex) {
-                    throw new RuntimeException(ex);
-                }
+        Thread t = new Thread(
+            () -> {
+                assertEquals("unknown", threadId.get());
+                threadId.set("worker");
             }
         );
+        t.start();
+        t.join();
+        assertEquals("main", threadId.get());
     }
 }
