7bb81ce6d4051121887ad30f21ee0cb0194bc141
Alex Dorogensky <alex@exebar.com>
2020-08-19 14:43:32 -0500

    Add pascal triangle challenge from Gentis Solution for 84.51

---
+++ src/main/java/training/problems/PascalTriangle.java
@@ -0,0 +1,32 @@
+package training.problems;
+
+/**
+ * Online test from Gentis Solutions for 84.51 8/19/2020
+ */
+public class PascalTriangle {
+
+    /*
+     * 1
+     * 1 1
+     * 1 2 1
+     * 1 3 3 1
+     */
+    public String print(int depth) {
+        int[][] x = new int[depth][depth];
+
+        StringBuilder triangle = new StringBuilder();
+
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) x[i][j] = 1;
+                else {
+                    x[i][j] = x[i - 1][j - 1] + x[i - 1][j];
+                }
+                triangle.append(x[i][j]);
+                triangle.append(" ");
+            }
+        }
+
+        return triangle.toString().trim();
+    }
+}
---
+++ src/test/java/training/problems/PascalTriangleTests.java
@@ -0,0 +1,38 @@
+package training.problems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PascalTriangleTests {
+
+    PascalTriangle pascalTriangle = new PascalTriangle();
+
+    /*
+     depth = 4
+        1
+       1 1
+      1 2 1
+     1 3 3 1
+     Print: 1 1 1 1 2 1 1 3 3 1
+    */
+    @Test
+    public void test() {
+        assertEquals("1 1 1 1 2 1 1 3 3 1", pascalTriangle.print(4));
+    }
+
+    /*
+     depth = 6
+              1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+    1   5  10   10  5  1
+    Print: 1 1 1 1 2 1 1 3 3 1 1 4 6 4 1 1 5 10 10 5 1
+    */
+    @Test
+    public void test2() {
+        assertEquals("1 1 1 1 2 1 1 3 3 1 1 4 6 4 1 1 5 10 10 5 1", pascalTriangle.print(6));
+    }
+}
