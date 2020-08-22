e78af3ef067262124b2533a3e47283578fef4b7f
Alex Dorogensky <alex@exebar.com>
2020-08-21 19:38:37 -0500

    MergeSort: (tdd.5) two elements in descending order test now passes

--- src/main/java/training/algorithms/MergeSort.java
+++ src/main/java/training/algorithms/MergeSort.java
@@ -3,6 +3,12 @@ package training.algorithms;
 public class MergeSort {
 
     public int[] sort(int... array) {
+        if (array.length == 1) return array;
+        else if (array[0] > array[1]) {
+            int array0 = array[0];
+            array[0] = array[1];
+            array[1] = array0;
+        }
         return array;
     }
 }
