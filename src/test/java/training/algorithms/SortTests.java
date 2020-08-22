cbb89b53e16d10931eae2c181a1339bc8912c7b3
Alex Dorogensky <alex@exebar.com>
2020-08-21 19:32:16 -0500

    SortTests: (tdd.3) add two elements in ascending order test that passes

--- src/test/java/training/algorithms/SortTests.java
+++ src/test/java/training/algorithms/SortTests.java
@@ -6,10 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SortTests {
 
-    private MergeSort sort = new MergeSort();
+    private final MergeSort sort = new MergeSort();
 
     @Test
     public void oneElementSort_shouldReturn_thatOneElement() {
         assertArrayEquals(new int[] { 1 }, sort.sort(1));
+    }
+
+    @Test
+    public void twoElementsInAscendingOrderSort_shouldReturn_thoseTwoElementsInSameOrder() {
+        assertArrayEquals(new int[] { 1, 2 }, sort.sort(1, 2));
     }
 }
