e3fec31d6bc0cd704ef90bcf46cccc8d157d7671
Alex Dorogensky <alex@exebar.com>
2020-08-21 20:10:41 -0500

    MergeSort: (tdd.8) three elements in descending order test now passes

--- src/main/java/training/algorithms/MergeSort.java
+++ src/main/java/training/algorithms/MergeSort.java
@@ -1,13 +1,32 @@
 package training.algorithms;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public int[] sort(int... array) {
         if (array.length == 1) return array;
-        else if (array[0] > array[1]) {
+        else if (array.length == 2 && array[0] > array[1]) {
             int array0 = array[0];
             array[0] = array[1];
             array[1] = array0;
+        } else if (array.length == 3) {
+            // array[0] & array[1..2]
+            int[] leftHalf = sort(Arrays.copyOfRange(array, 0, array.length / 2));
+            int[] rightHalf = sort(Arrays.copyOfRange(array, array.length / 2, array.length));
+            int leftHalfIdx = 0, rightHalfIdx = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                if (leftHalfIdx == leftHalf.length) {
+                    array[i] = rightHalf[rightHalfIdx++];
+                } else if (rightHalfIdx == rightHalf.length) {
+                    array[i] = leftHalf[leftHalfIdx++];
+                } else if (leftHalf[leftHalfIdx] > rightHalf[rightHalfIdx]) {
+                    array[i] = rightHalf[rightHalfIdx++];
+                } else {
+                    array[i] = leftHalf[leftHalfIdx++];
+                }
+            }
         }
         return array;
     }
