bceabd1ef2721390505425c493f374730628cc2c
Alex Dorogensky <alex@exebar.com>
2020-08-25 21:57:14 -0500

    configure warm up and measurement iterations

--- src/benchmarks/java/training/benchmarks/MyBenchmarks.java
+++ src/benchmarks/java/training/benchmarks/MyBenchmarks.java
@@ -4,11 +4,14 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
+@State(Scope.Thread)
+@BenchmarkMode(Mode.SingleShotTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(value = 1)
 public class MyBenchmarks {
 
     @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 10, time = 10, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 10, time = 10, timeUnit = TimeUnit.MILLISECONDS)
     public void do_nothing() {}
 }
