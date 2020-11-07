bc9b14a30de6233b6912d49bf506a954d32700e5
Alex Dorogensky <alex@exebar.com>
2020-11-07 16:17:12 -0600

    MergeSort.java: change variable names

--- src/main/java/training/algorithms/QuickSort.java
+++ src/main/java/training/algorithms/QuickSort.java
@@ -8,29 +8,29 @@ public class QuickSort {
 
     private int[] sort(int[] array, int leftIdx, int rightIdx) {
         if (leftIdx >= rightIdx) return array;
-        int pivotIdx = partition(array, leftIdx, rightIdx);
-        sort(array, leftIdx, pivotIdx - 1);
-        sort(array, pivotIdx + 1, rightIdx);
+        int partitionIdx = partition(array, leftIdx, rightIdx);
+        sort(array, leftIdx, partitionIdx - 1);
+        sort(array, partitionIdx + 1, rightIdx);
         return array;
     }
 
     private int partition(int[] array, int leftIdx, int rightIdx) {
         int pivot = array[leftIdx];
-        int partitionIdx = leftIdx + 1;
+        int pivotIdx = leftIdx + 1;
 
-        for (int unseenElementIdx = leftIdx + 1; unseenElementIdx <= rightIdx; unseenElementIdx++) {
-            if (array[unseenElementIdx] < pivot) {
-                int elementAtPartitionIdx = array[partitionIdx];
-                array[partitionIdx++] = array[unseenElementIdx];
-                array[unseenElementIdx] = elementAtPartitionIdx;
+        for (int i = pivotIdx; i <= rightIdx; i++) {
+            if (array[i] < pivot) {
+                int elementAtPivotIdx = array[pivotIdx];
+                array[pivotIdx++] = array[i];
+                array[i] = elementAtPivotIdx;
             }
         }
 
-        partitionIdx--;
+        pivotIdx--;
 
-        array[leftIdx] = array[partitionIdx];
-        array[partitionIdx] = pivot;
+        array[leftIdx] = array[pivotIdx];
+        array[pivotIdx] = pivot;
 
-        return partitionIdx;
+        return pivotIdx;
     }
 }
