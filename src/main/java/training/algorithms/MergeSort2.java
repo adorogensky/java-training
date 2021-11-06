6c62aa041c901d1f44fb422d7ece0953920212c4
Alex Dorogensky <alex@exebar.com>
2021-11-05 18:45:44 -0700

    MergeSort2.3 passing

--- src/main/java/training/algorithms/MergeSort2.java
+++ src/main/java/training/algorithms/MergeSort2.java
@@ -1,32 +1,27 @@
 package training.algorithms;
 
+import java.util.Arrays;
+
 public class MergeSort2 {
 
     public int[] sort(int... array) {
-        return sort(array, 0, array.length);
-    }
-
-    private int[] sort(int[] array, int startIdx, int endIdx) {
-        if (startIdx >= endIdx) {
+        if (array.length == 1) {
             return array;
         }
 
-        int midIdx = (endIdx - startIdx - 1) / 2;
-        int[] leftSortedArray = sort(array, startIdx, midIdx);
-        int[] rightSortedArray = sort(array, midIdx, endIdx);
+        int[] leftHalfSorted = sort(Arrays.copyOfRange(array, 0, array.length / 2));
+        int[] rightHalfSorted = sort(Arrays.copyOfRange(array, array.length / 2, array.length));
 
-        int leftArrayIdx = 0;
-        int rightArrayIdx = 0;
-
-        for (int i = startIdx; i < endIdx; i++) {
-            if (leftArrayIdx == leftSortedArray.length) {
-                array[i] = rightSortedArray[rightArrayIdx++];
-            } else if (rightArrayIdx == rightSortedArray.length) {
-                array[i] = leftSortedArray[leftArrayIdx++];
-            } else if (rightSortedArray[rightArrayIdx] < leftSortedArray[leftArrayIdx]) {
-                array[i] = rightSortedArray[rightArrayIdx++];
+        int leftHalfIdx = 0, rightHalfIdx = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (leftHalfIdx == leftHalfSorted.length) {
+                array[i] = rightHalfSorted[rightHalfIdx++];
+            } else if (rightHalfIdx == rightHalfSorted.length) {
+                array[i] = leftHalfSorted[leftHalfIdx++];
+            } else if (rightHalfSorted[rightHalfIdx] < leftHalfSorted[leftHalfIdx]) {
+                array[i] = rightHalfSorted[rightHalfIdx++];
             } else {
-                array[i] = leftSortedArray[leftArrayIdx++];
+                array[i] = leftHalfSorted[leftHalfIdx++];
             }
         }
 
