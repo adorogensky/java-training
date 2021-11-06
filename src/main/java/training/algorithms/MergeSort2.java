337fb30421509a7b5c9cbff69097d7b6b2876ae6
Alex Dorogensky <alex@exebar.com>
2021-11-05 18:24:28 -0700

    MergeSort2.1 failing

--- src/main/java/training/algorithms/MergeSort2.java
+++ src/main/java/training/algorithms/MergeSort2.java
@@ -3,6 +3,32 @@ package training.algorithms;
 public class MergeSort2 {
 
     public int[] sort(int... array) {
-        return null;
+        return sort(array, 0, array.length);
+    }
+
+    private int[] sort(int[] array, int startIdx, int endIdx) {
+        if (startIdx >= endIdx) {
+            return array;
+        }
+
+        int[] leftSortedArray = sort(array, 0, array.length / 2);
+        int[] rightSortedArray = sort(array, array.length / 2, array.length);
+
+        int leftArrayIdx = 0;
+        int rightArrayIdx = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (leftArrayIdx == leftSortedArray.length) {
+                array[i] = rightSortedArray[rightArrayIdx++];
+            } else if (rightArrayIdx == rightSortedArray.length) {
+                array[i] = leftSortedArray[leftArrayIdx++];
+            } else if (rightSortedArray[rightArrayIdx] < leftSortedArray[leftArrayIdx]) {
+                array[i] = rightSortedArray[rightArrayIdx++];
+            } else {
+                array[i] = leftSortedArray[leftArrayIdx++];
+            }
+        }
+
+        return array;
     }
 }
--- src/test/java/training/algorithms/SortTests.java
+++ src/test/java/training/algorithms/SortTests.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SortTests {
 
-    private final QuickSort sort = new QuickSort();
+    private final MergeSort2 sort = new MergeSort2();
 
     @Test
     public void oneElementSort_shouldReturn_thatOneElement() {
