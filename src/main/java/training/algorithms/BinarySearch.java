5aec578db47abe3da8198d176885330b5fc954d5
Alex Dorogensky <alex@exebar.com>
2021-11-24 18:05:20 -0800

    [7m] - binary search both asc and desc (no prep), + 6 more test cases

--- src/main/java/training/algorithms/BinarySearch.java
+++ src/main/java/training/algorithms/BinarySearch.java
@@ -2,12 +2,17 @@ package training.algorithms;
 
 public class BinarySearch {
 
-    public int doSearch(int[] a, int el) {
-        return doSearch(a, el, 0, a.length - 1);
+    public enum Order {
+        ASC, DESC
     }
 
-    // assume a is sorted in ascending order
-    public int doSearch(int[] a, int el, int sIdx, int eIdx) {
+    public int doSearch(int[] a, int el) {
+        Order o = a[0] < a[a.length - 1] ? Order.ASC : Order.DESC;
+        return doSearch(a, o, el, 0, a.length - 1);
+    }
+
+    // assume a is sorted in ascending or descending order
+    public int doSearch(int[] a, Order o, int el, int sIdx, int eIdx) {
         if (sIdx > eIdx) {
             return -1;
         }
@@ -16,23 +21,21 @@ public class BinarySearch {
             return a[sIdx] == el ? sIdx : -1;
         }
 
-        if (a[sIdx] > el || a[eIdx] < el) {
+        if (o == Order.ASC && (a[sIdx] > el || a[eIdx] < el)) {
+            return -1;
+        } else if (o == Order.DESC && (a[sIdx] < el || a[eIdx] > el)) {
             return -1;
         }
 
         int mIdx = sIdx + (eIdx - sIdx) / 2;
 
-        int lr = doSearch(a, el, sIdx, mIdx);
-        int rr = doSearch(a, el,mIdx + 1, eIdx);
+        int lr = doSearch(a, o, el, sIdx, mIdx);
+        int rr = doSearch(a, o, el, mIdx + 1, eIdx);
 
         if (lr > -1) {
             return lr;
         }
 
-        if (rr > -1) {
-            return rr;
-        }
-
-        return -1;
+        return Math.max(rr, -1);
     }
 }
--- src/test/java/training/algorithms/BinarySearchTests.java
+++ src/test/java/training/algorithms/BinarySearchTests.java
@@ -13,6 +13,10 @@ public class BinarySearchTests {
         assertEquals(0, bs.doSearch(new int[] { 1 }, 1));
         assertEquals(-1, bs.doSearch(new int[] { 1 }, 2));
         assertEquals(3, bs.doSearch(new int[] { 1, 2, 4, 7, 9, 10 }, 7));
+        assertEquals(2, bs.doSearch(new int[] { 1, 2, 4, 7, 9 }, 4));
         assertEquals(-1, bs.doSearch(new int[] { 1, 2, 4, 7, 9, 10 }, 11));
+        assertEquals(2, bs.doSearch(new int[] { 10, 9, 7, 4, 2, 1 }, 7));
+        assertEquals(2, bs.doSearch(new int[] { 9, 7, 4, 2, 1 }, 4));
+        assertEquals(-1, bs.doSearch(new int[] { 10, 9, 7, 4, 2, 1 }, 11));
     }
 }
